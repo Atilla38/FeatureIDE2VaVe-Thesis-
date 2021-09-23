61f532cd1c0bad088c75985f23595758ca360ad0
Atilla Ates <usxoi@student.kit.edu>
2021-09-23 17:53:56 +0200

    added the FeatureIDE example projects to the repository

---
+++ FeatureIDE-Projects/LibSSH-Colligens/build/dh_preprocessed.c
@@ -0,0 +1,8 @@
+void sig_verify ( int gcry, int valid ) {
+ int BAD_SIGN = 0;
+    // Code here..
+  if (gcry != BAD_SIGN) {
+   set_error(2);
+  // Several lines of code here..
+ } //if neither LIBGCRYPT nor LIBCRYPTO is activated we will have the '}' without the opening '{'
+}
---
+++ FeatureIDE-Projects/LibSSH-Colligens/build/main_preprocessed.c
@@ -0,0 +1,7 @@
+void free (char* hex){
+ // Do nothing..
+}
+void ssh_print_bignum(){
+    unsigned char *hex;
+ free(hex);
+}
