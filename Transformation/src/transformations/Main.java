d91271e5d53ad6a30c643543fa99c57fca37fed2
Atilla Ates <usxoi@student.kit.edu>
2021-10-13 19:38:10 +0200

    Implemented Main and XSD2VaveTranformation.
    Implemented the parse-Methods in the XSD2Tranformation class.
    Implemented the main method in the main class to load xml files and transform them with the transformations calsses.

--- Transformation/src/transformations/Main.java
+++ Transformation/src/transformations/Main.java
@@ -1,10 +1,42 @@
 package transformations;
 
+import java.io.File;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+
+import FeatureIDEXSD.DocumentRoot;
+import FeatureIDEXSD.FeatureModelType;
+import FeatureIDEXSD.StructType;
+import FeatureIDEXSD.util.FeatureIDEXSDResourceFactoryImpl;
+
 public class Main {
+	static XSD2VaveTransformation transformation;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		File file = new File("D:\\Projekte\\Feature\\FeatureIDE\\FeatureIDE.xml");
+		transformation = new XSD2VaveTransformation();
+		Resource resource = loadFeatureIDEXMLFile(file);
+		DocumentRoot root = (DocumentRoot) resource.getContents().get(0);
 
+		if (root.getFeatureModel() != null) {
+			FeatureModelType featurModel = root.getFeatureModel();
+			StructType struct = featurModel.getStruct();
+			
+			transformation.start(struct);
+		}
+
+	}
+
+	public static Resource loadFeatureIDEXMLFile(File musicXMLFile) {
+		ResourceSetImpl resourceSet = new ResourceSetImpl();
+		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xml",
+				new FeatureIDEXSDResourceFactoryImpl());
+		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("featureIDExml",
+				new FeatureIDEXSDResourceFactoryImpl());
+
+		return resourceSet.getResource(URI.createFileURI(musicXMLFile.getAbsolutePath()), true);
 	}
 
 }
