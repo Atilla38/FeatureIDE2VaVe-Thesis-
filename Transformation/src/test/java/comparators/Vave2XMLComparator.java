dad3bc01ec3c9ee5e16023dc6448853e0c829b97
Atilla Ates <usxoi@student.kit.edu>
2021-11-28 15:12:06 +0100

    Added documentation.

--- Transformation/src/test/java/comparators/Vave2XMLComparator.java
+++ Transformation/src/test/java/comparators/Vave2XMLComparator.java
@@ -7,11 +7,21 @@ import java.util.List;
 
 import transformations.Main;
 
+/**
+ * Compares two XML-Files.
+ * 
+ * @author Atilla
+ *
+ */
 public class Vave2XMLComparator extends XMLComparator {
 	protected String sourceFolderFeatureIDE = "src/test/resource/vave2xml/FeatureIDE/";
 	private String sourceFolderVave = "src/test/resource/vave2xml/vave/";
 	protected String targetFolderFeatureIDE = "target/src/test/resource/models/FeatureIDE/";
 
+	/**
+	 * Generates FeatureIDE Feature-Model XML-Files, by transforming VaVemodels in
+	 * the source VaVe folder(default: src/test/resource/vave2xml/vave/).
+	 */
 	public void generateVave2XMLFiles() {
 		addFilesToList(fileList, "xml", sourceFolderFeatureIDE); // XML files on which the self created vavemodels are
 																	// based.
