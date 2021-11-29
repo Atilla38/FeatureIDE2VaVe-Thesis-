736c9a1954d1005c28aad892018de6f9b1258275
Atilla Ates <usxoi@student.kit.edu>
2021-11-29 20:05:17 +0100

    Added some test resources for the change resolution test.

--- Transformation/src/test/java/changeResolution/test/ChangeResolutionTest.java
+++ Transformation/src/test/java/changeResolution/test/ChangeResolutionTest.java
@@ -10,7 +10,6 @@ import tools.vitruv.framework.domains.DefaultStateBasedChangeResolutionStrategy;
 import tools.vitruv.framework.domains.StateBasedChangeResolutionStrategy;
 import transformations.Main;
 
-
 class ChangeResolutionTest {
 
 	@SuppressWarnings("static-access")
@@ -18,14 +17,18 @@ class ChangeResolutionTest {
 	void testDefaultStateBasedChangeResolutionStrategy() {
 		Main main = new Main();
 		StateBasedChangeResolutionStrategy strategy = new DefaultStateBasedChangeResolutionStrategy();
-		File featureIDEOldStateFile = new File("src/test/resource/changeResolution/FeatureIDE/oldState/car2.xml");
-		File featureIDENewStateFile = new File("src/test/resource/changeResolution/FeatureIDE/newState/car2.xml");
-		Resource oldState = main.generateVaveModel(featureIDEOldStateFile, "car", "src/test/resource/changeResolution/vave/oldState/");
-		Resource newState = main.generateVaveModel(featureIDENewStateFile, "car", "src/test/resource/changeResolution/vave/newState/");
-		
+
+		File featureIDEOldStateFile = new File("src/test/resource/changeResolution/FeatureIDE/oldState/oldStateCar.xml");
+		File featureIDENewStateFile = new File("src/test/resource/changeResolution/FeatureIDE/newState/newStateCar.xml");
+
+		Resource oldState = main.generateVaveModel(featureIDEOldStateFile, "oldStateCar",
+				"src/test/resource/changeResolution/vave/oldState/");
+		Resource newState = main.generateVaveModel(featureIDENewStateFile, "newStateCar",
+				"src/test/resource/changeResolution/vave/newState/");
+
 		VitruviusChange change = strategy.getChangeSequenceBetween(newState, oldState);
 		System.out.println(change);
-		
+
 	}
 
 }
