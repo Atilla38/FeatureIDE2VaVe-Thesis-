06cdee987b5312eb258630fb33d46f3cd732a160
Atilla Ates <usxoi@student.kit.edu>
2021-11-29 19:54:38 +0100

    Update ChangeResolutionTest.java

--- Transformation/src/test/java/changeResolution/test/ChangeResolutionTest.java
+++ Transformation/src/test/java/changeResolution/test/ChangeResolutionTest.java
@@ -13,13 +13,13 @@ import transformations.Main;
 
 class ChangeResolutionTest {
 
+	@SuppressWarnings("static-access")
 	@Test
 	void testDefaultStateBasedChangeResolutionStrategy() {
 		Main main = new Main();
 		StateBasedChangeResolutionStrategy strategy = new DefaultStateBasedChangeResolutionStrategy();
 		File featureIDEOldStateFile = new File("src/test/resource/changeResolution/FeatureIDE/oldState/car2.xml");
 		File featureIDENewStateFile = new File("src/test/resource/changeResolution/FeatureIDE/newState/car2.xml");
-		
 		Resource oldState = main.generateVaveModel(featureIDEOldStateFile, "car", "src/test/resource/changeResolution/vave/oldState/");
 		Resource newState = main.generateVaveModel(featureIDENewStateFile, "car", "src/test/resource/changeResolution/vave/newState/");
 		
