94629c3894818b26cec3afddf358cf6148c7637c
Atilla Ates <usxoi@student.kit.edu>
2021-11-29 19:53:11 +0100

    Implemented test case to test the DefaultStateBasedChangeResolutionStrategy from Vitruvius.

---
+++ Transformation/src/test/java/changeResolution/test/ChangeResolutionTest.java
@@ -0,0 +1,31 @@
+package changeResolution.test;
+
+import java.io.File;
+
+import org.eclipse.emf.ecore.resource.Resource;
+import org.junit.jupiter.api.Test;
+
+import tools.vitruv.framework.change.description.VitruviusChange;
+import tools.vitruv.framework.domains.DefaultStateBasedChangeResolutionStrategy;
+import tools.vitruv.framework.domains.StateBasedChangeResolutionStrategy;
+import transformations.Main;
+
+
+class ChangeResolutionTest {
+
+	@Test
+	void testDefaultStateBasedChangeResolutionStrategy() {
+		Main main = new Main();
+		StateBasedChangeResolutionStrategy strategy = new DefaultStateBasedChangeResolutionStrategy();
+		File featureIDEOldStateFile = new File("src/test/resource/changeResolution/FeatureIDE/oldState/car2.xml");
+		File featureIDENewStateFile = new File("src/test/resource/changeResolution/FeatureIDE/newState/car2.xml");
+		
+		Resource oldState = main.generateVaveModel(featureIDEOldStateFile, "car", "src/test/resource/changeResolution/vave/oldState/");
+		Resource newState = main.generateVaveModel(featureIDENewStateFile, "car", "src/test/resource/changeResolution/vave/newState/");
+		
+		VitruviusChange change = strategy.getChangeSequenceBetween(newState, oldState);
+		System.out.println(change);
+		
+	}
+
+}
